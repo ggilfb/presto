4705e8a48bf281b750985fb3ce3f17fb9382f210
German Gil <ggil@fb.com>
2017-10-27 10:02:27 -0700

    Fix comment on digest memory estimation

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/state/TDigestAndPercentileArrayStateFactory.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/state/TDigestAndPercentileArrayStateFactory.java
@@ -144,10 +144,10 @@ public class TDigestAndPercentileArrayStateFactory
         public long getEstimatedSize()
         {
             long estimatedSize = INSTANCE_SIZE;
-            if (digest != null) {
-                // TBD: To fix, could we get a proper memory estimation?
+            // TBD: To fix, could we get a proper memory estimation?
+            /*if (digest != null) {
                 estimatedSize += 0; // digest.estimatedInMemorySizeInBytes();
-            }
+            }*/
             if (percentiles != null) {
                 estimatedSize += SizeOf.sizeOfDoubleArray(percentiles.size());
             }
